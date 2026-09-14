6b19dd5069660708ddeb2bc8e3844ee0d98a7954
zahidkaya1 <zahidkaya64@gmail.com>
2026-09-14 17:19:40 +0300

    Windows masaüstü uygulaması Tauri ile entegre edildi

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+const host = process.env.TAURI_DEV_HOST
+
 export default defineConfig({
   plugins: [react()],
+
+  clearScreen: false,
+
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: host || false,
+
+    hmr: host
+      ? {
+        protocol: 'ws',
+        host,
+        port: 1421,
+      }
+      : undefined,
+
+    watch: {
+      ignored: ['**/src-tauri/**'],
+    },
+  },
+
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
 })
